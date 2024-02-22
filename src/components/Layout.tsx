df8cb6db9f34b1645738eef4edac6277f60b93ad
Alexander Fejari <alexander.fejari@gmail.com>
2024-02-22 18:37:30 +0100

    mobile done wip on desktop hover

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,31 @@
-// Layout.tsx
-import {ReactNode} from "react";
+import { ReactNode, useState, useEffect } from "react";
 import NavBar from "@/components/ui/NavBar";
+import { useToggle } from '@/hooks/useToggle'; // Importez le hook useToggle
 
 interface LayoutProps {
     children: ReactNode;
 }
 
-const Layout = ({children}: LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
+    const { isActive: toggleIsActive, toggleMenu } = useToggle(); // Utilisez le hook useToggle
+
+    // Utilisez un état local pour gérer l'état du menu dans le Layout
+    const [isActive, setIsActive] = useState(toggleIsActive);
+
+    // Utilisez useEffect pour synchroniser l'état local avec l'état du menu
+    useEffect(() => {
+        setIsActive(toggleIsActive);
+    }, [toggleIsActive]);
+
     return (
         <>
-            <section className='w-full h-dvh'>
+            <section className={`w-full h-dvh`}>
                 <header
                     className={
-                        "min-h-20 max-h-28 p-6 w-full fixed top-0 left-0 backdrop-blur-lg"
+                        `min-h-20 max-h-28 p-6 w-full fixed top-0 left-0 flex flex-row items-center transition-all linear duration-75 delay-100 ${isActive ? '' : 'backdrop-blur-md'}`
                     }
                 >
-                    <NavBar/>
+                    <NavBar isActive={isActive} toggleMenu={toggleMenu} /> {/* Passez isActive et toggleMenu au composant NavBar */}
                 </header>
                 <main className={' p-6'}>
                     {children}
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,13 +1,17 @@
-// NavBar.tsx
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import Logo from "@/components/ui/Logo";
 import {ModeToggle} from "@/components/mode-toggle";
 import NavBarLinks from "@/components/ui/NavBarLinks";
 import {NavItem} from '@/types/types';
 import MenuIcon from "@/components/ui/MenuIcon";
-import {useToggle} from '@/hooks/useToggle';
+import gsap from "gsap";
 
-const NavBar = () => {
+interface NavBarProps {
+    isActive: boolean;
+    toggleMenu: () => void;
+}
+
+const NavBar = ({isActive, toggleMenu}: NavBarProps) => {
     const navItems: NavItem[] = [
         {path: '/', label: 'Home'},
         {path: '/about', label: 'About me'},
@@ -15,15 +19,38 @@ const NavBar = () => {
         {path: '/contact', label: 'Contact'},
     ];
 
-    // Utilisation de votre hook useToggle pour gérer l'état d'ouverture/fermeture du menu
-    const {isActive, toggleMenu} = useToggle();
+    const containerRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        // Empêcher le défilement de la page lorsque le menu est ouvert
+        const tl = gsap.timeline({defaults: {ease: "power2.inOut"}});
+
         if (isActive) {
-            document.body.style.overflow = 'hidden';
+            tl.to(containerRef.current, {height: '100dvh', duration: 0.5, ease: 'power2.inOut'})
+                .to(containerRef.current, {
+                    backdropFilter: 'blur(12px)',
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                    duration: 0.5,
+                    ease: 'power2.inOut'
+                }, '-=0.5')
+                .to(containerRef.current, {opacity: 1, duration: 0.5, ease: 'power2.inOut'}, '-=0.5');
         } else {
-            document.body.style.overflow = '';
+            tl.to(containerRef.current, {
+                opacity: 0,
+                duration: 0.5,
+                ease: 'power2.inOut'
+            })
+                .to(containerRef.current, {
+                    height: 0,
+                    duration: 0.5,
+                    ease: 'power2.inOut'
+                    }, '-=0.5'
+                )
+                .to(containerRef.current, {
+                    backdropFilter: 'blur(0)',
+                    backgroundColor: 'transparent',
+                    duration: 0.5,
+                    ease: 'power2.inOut'
+                }, '-=0.5');
         }
     }, [isActive]);
 
@@ -33,28 +60,33 @@ const NavBar = () => {
                 <section className={'md:w-1/2 md:flex md:justify-start'}>
                     <Logo size={42}/>
                 </section>
-                <section className={'md:hidden'}>
+                <section className={'w-10 h-10 md:hidden'}>
                     <ModeToggle/>
                 </section>
                 {/* desktop */}
-                <section className={'md:w-1/2 hidden md:flex md:justify-end md:items-center md:gap-8'}>
-                    <section className={'w-auto md:flex md:justify-center'}>
-                        <NavBarLinks links={navItems}/>
+                <section className={'hidden md:w-1/2 md:flex md:justify-end md:items-center gap-16'}>
+                    <section className={'h-10 md:flex md:justify-center'}>
+                        <NavBarLinks isActive={isActive} toggleMenu={toggleMenu} links={navItems}/>
                     </section>
-                    <section className={'w-auto md:flex md:justify-center'}>
-                    <ModeToggle/>
-                </section>
+                    <section className={'w-10 h-10 md:flex md:justify-center md:items-center'}>
+                        <ModeToggle/>
+                    </section>
                 </section>
                 {/*Mobile*/}
                 <section className={'relative md:hidden'}>
                     {/* Passage de toggleMenu à MenuIcon */}
-                    <MenuIcon className={`${isActive ? 'relative z-20' : 'block'}`} size={42} toggleMenu={toggleMenu}
-                              isActive={isActive}/>
+                    <MenuIcon className={`${isActive ? 'relative z-20' : 'block'}`}
+                              size={42}
+                              toggleMenu={toggleMenu}
+                              isActive={isActive}
+                    />
                     {/* Affichage conditionnel des NavBarLinks basé sur isActive */}
                     <section
-                        className={`fixed w-full top-0 left-0 z-10 transition-all ease-in-out delay-300 duration-500 ${isActive ? 'h-dvh bg-opacity-50 backdrop-blur-lg' : 'h-0 bg-transparent backdrop-blur-0'}`}>
+                        ref={containerRef}
+                        className={`fixed w-full top-0 left-0 z-10 transition-all`}
+                    >
                         {isActive && (
-                            <NavBarLinks links={navItems}/>
+                            <NavBarLinks isActive={isActive} toggleMenu={toggleMenu} links={navItems}/>
                         )}
                     </section>
                 </section>
